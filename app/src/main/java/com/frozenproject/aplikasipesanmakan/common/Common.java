4e8d366d3ef457fdde3e3c5711b074b6cc0569e7
Reno Kurniawan <renokurniawan730@gmail.com>
2019-10-25 19:04:13 +0700

    initial commit

---
+++ app/src/main/java/com/frozenproject/aplikasipesanmakan/common/Common.java
@@ -0,0 +1,8 @@
+package com.frozenproject.aplikasipesanmakan.common;
+
+import com.frozenproject.aplikasipesanmakan.model.UsersModel;
+
+public class Common {
+    public static String USER_REFERENCES = "Users";
+    public static UsersModel currentUser;
+}
---
+++ app/src/main/java/com/frozenproject/aplikasipesanmakan/remote/RetrofitCloudClient.java
@@ -0,0 +1,19 @@
+package com.frozenproject.aplikasipesanmakan.remote;
+
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+public class RetrofitCloudClient {
+    private static Retrofit instance;
+    public static Retrofit getInstance(){
+        if(instance == null)
+            instance = new Retrofit.Builder()
+                    .baseUrl("https://us-central1-menumakanan-fbf35.cloudfunctions.net/")
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                    .build();
+
+        return instance;
+    }
+}
